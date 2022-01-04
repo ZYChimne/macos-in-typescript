f21332f6a6c875982130e9248f057c72fca8f03e
Cirilla <gazlayxyc@outlook.com>
2022-01-04 17:44:07 +0800

    update

--- src/components/app/reminders/reminders.d.tsx
+++ src/components/app/reminders/reminders.d.tsx
@@ -4,3 +4,14 @@ export type RemindersProps = {
   show: boolean;
   setApp: React.Dispatch<AppLoads>;
 };
+export type TagContainerProps = {
+  id: string;
+  curTag: string;
+  setCurTag: (value: React.SetStateAction<string>) => void;
+};
+export const TagColors: { [key: string]: string } = {
+  Today: `rgb(48, 123, 246)`,
+  Scheduled: `rgb(236, 77, 61)`,
+  All: `rgb(92, 98, 105)`,
+  Flagged: `rgb(241, 153, 56)`,
+};
--- src/components/app/reminders/reminders.tsx
+++ src/components/app/reminders/reminders.tsx
@@ -1,19 +1,117 @@
-import React from 'react';
+import {
+  faCalendarAlt,
+  faCalendarDay,
+  faFlag,
+  faInbox,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { AppBarButton } from '../../../utils/utlils';
-import { RemindersProps } from './reminders.d';
+import { RemindersProps, TagColors, TagContainerProps } from './reminders.d';
 import styles from './reminders.module.scss';
 export const Reminders = (props: RemindersProps) => {
+  const [curTag, setCurTag] = useState('Today');
   return (
     <div className={styles.reminders} data-show={props.show}>
       <div className={styles.appBar}>
         <div className={styles.appBarBtnContainer}>
           <AppBarButton setClose={() => props.setApp('Reminders')} />
         </div>
+        <input className={styles.searchbar} type="text" />
+        <div className={styles.appBarLine}>
+          <TagContainer id="Today" curTag={curTag} setCurTag={setCurTag} />
+          <TagContainer id="Scheduled" curTag={curTag} setCurTag={setCurTag} />
+        </div>
+        <div className={styles.appBarLine}>
+          <TagContainer id="All" curTag={curTag} setCurTag={setCurTag} />
+          <TagContainer id="Flagged" curTag={curTag} setCurTag={setCurTag} />
+        </div>
       </div>
       <div className={styles.content}>
         <div className={styles.remindersBar}></div>
         <div className={styles.remindersContent}>
-          <div>Coming Soon</div>
+          <div
+            className={styles.remindersContentTitle}
+            style={{ color: TagColors[curTag] }}
+          >
+            {curTag}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+const TagContainer = (props: TagContainerProps) => {
+  let tagIcon;
+  const active = props.id === props.curTag;
+  switch (props.id) {
+    case 'Today':
+      tagIcon = (
+        <FontAwesomeIcon
+          className={styles.tagIcon}
+          icon={faCalendarDay}
+          style={{
+            color: active ? TagColors[props.id] : `#FFFFFF`,
+          }}
+        />
+      );
+      break;
+    case 'Scheduled':
+      tagIcon = (
+        <FontAwesomeIcon
+          className={styles.tagIcon}
+          icon={faCalendarAlt}
+          style={{
+            color: active ? TagColors[props.id] : `#FFFFFF`,
+          }}
+        />
+      );
+      break;
+    case 'All':
+      tagIcon = (
+        <FontAwesomeIcon
+          className={styles.tagIcon}
+          icon={faInbox}
+          style={{
+            color: active ? TagColors[props.id] : `#FFFFFF`,
+          }}
+        />
+      );
+      break;
+    case 'Flagged':
+      tagIcon = (
+        <FontAwesomeIcon
+          className={styles.tagIcon}
+          icon={faFlag}
+          style={{
+            color: active ? TagColors[props.id] : `#FFFFFF`,
+          }}
+        />
+      );
+      break;
+  }
+  return (
+    <div
+      className={styles.tagContainer}
+      onClick={() => props.setCurTag(props.id)}
+      style={{
+        background: active ? TagColors[props.id] : `rgba(207, 206, 206, 1)`,
+      }}
+    >
+      <div className={styles.tagLeftContainer}>
+        <div
+          className={styles.tagIconBox}
+          style={{ background: active ? `#FFFFFF` : TagColors[props.id] }}
+        >
+          {tagIcon}
+        </div>
+        <div className={styles.tagText} data-active={active}>
+          {props.id}
+        </div>
+      </div>
+      <div className={styles.tagRightContainer}>
+        <div className={styles.tagNumber} data-active={active}>
+          0
         </div>
       </div>
     </div>
